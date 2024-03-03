8625ac7f660ceb6188a78e9fc5f2191df051e663
David Javakhishvili <david@underscore.dev>
2024-03-03 10:17:45 +0000

    Add date to the version

--- packages/default.nix
+++ packages/default.nix
@@ -27,5 +27,6 @@
     mali-firmware-g610-g21p0-01eac0
     libmali-valhall-g610-g13p0-x11-wayland-gbm;
 
-  mesa = (pkgs.callPackage ./mesa.nix {});
+  mesa = mesa-24_0_99-2024_03_04;
+  mesa-24_0_99-2024_03_04 = (pkgs.callPackage ./mesa.nix {});
 }
