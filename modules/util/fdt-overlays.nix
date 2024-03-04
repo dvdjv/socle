6f8fbfbbbe89f7848f258ae007eb823e4d873479
David Javakhishvili <david@underscore.dev>
2024-03-04 21:21:49 +0000

    Fix options path

--- modules/util/fdt-overlays.nix
+++ modules/util/fdt-overlays.nix
@@ -4,18 +4,20 @@
   elCfg = blCfg.generic-extlinux-compatible;
   timeoutStr = if blCfg.timeout == null then "-1" else toString blCfg.timeout;
 in {
-  options.hardware.enabledOverlays = with lib; mkOption {
-    default = [];
-    example = literalExpression ''
-    [
-      "rockchip/overlay/rk3588-disable-led.dtbo"
-      "rockchip/overlay/rk3588-wifi-ap6275p.dtbo"
-    ]
-    '';
-    type = types.listOf .types.str;
-    description = mdDoc ''
-    List of overlays to apply at runtime, relative to the dtb base.
-    '';
+  options = {
+      hardware.deviceTree.enabledOverlays = with lib; mkOption {
+      default = [];
+      example = literalExpression ''
+      [
+        "rockchip/overlay/rk3588-disable-led.dtbo"
+        "rockchip/overlay/rk3588-wifi-ap6275p.dtbo"
+      ]
+      '';
+      type = types.listOf .types.str;
+      description = mdDoc ''
+      List of overlays to apply at runtime, relative to the dtb base.
+      '';
+    };
   };
 
   config = let
