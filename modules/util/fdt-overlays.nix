93d3692c2eeb7fdc7c4794ca1d43d7e254fec3a0
David Javakhishvili <610947+dvdjv@users.noreply.github.com>
2024-03-06 11:06:31 +0000

    Fix broken extlinux file builder

--- modules/util/fdt-overlays.nix
+++ modules/util/fdt-overlays.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }: let
   dtCfg = config.hardware.deviceTree;
-  blCfg = config.boot.loader;
-  elCfg = blCfg.generic-extlinux-compatible;
-  timeoutStr = if blCfg.timeout == null then "-1" else toString blCfg.timeout;
 in {
+  disabledModules = [ "system/boot/loader/generic-extlinux-compatible" ];
+  imports = [ ./generic-extlinux-compatible ];
+
   options = {
       hardware.deviceTree.enabledOverlays = with lib; mkOption {
       default = [];
@@ -20,22 +20,9 @@ in {
     };
   };
 
-  config = let
-    populateBuilder = pkgs.substituteAll {
-      src = ./extlinux-conf-builder.sh;
-      isExecutable = true;
-      path = [pkgs.coreutils pkgs.gnused pkgs.gnugrep];
-      inherit (pkgs) bash;
-    } { pkgs = pkgs.buildPackages; };
-
-    builderArgs = "-g ${toString elCfg.configurationLimit} -t ${timeoutStr}"
-      + lib.optionalString (dtCfg.name != null) " -n ${dtCfg.name}"
-      + lib.optionalString (!elCfg.useGenerationDeviceTree) " -r";
-  in lib.mkIf (dtCfg.enable) {
+  config = lib.mkIf (dtCfg.enable) {
     system.extraSystemBuilderCmds = ''
         echo ${builtins.concatStringsSep " " dtCfg.enabledOverlays } > $out/devicetree-overlays
       '';
-
-    boot.loader.generic-extlinux-compatible.populateCmd = "${populateBuilder} ${builderArgs}";
   };
 }
