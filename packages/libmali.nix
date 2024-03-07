2a6034a40711fa400b0f7dd41b96d998dc2eb5e2
David Javakhishvili <david@underscore.dev>
2024-03-07 12:32:59 +0000

    Switch to x11-wayland-gbm variant

--- packages/libmali.nix
+++ packages/libmali.nix
@@ -1,6 +1,6 @@
-{ fetchFromGitHub, stdenv, autoPatchelfHook, libdrm, ... }: stdenv.mkDerivation {
-    pname = "libmali";
-    version = "g610-g6p0";
+{ fetchFromGitHub, stdenv, autoPatchelfHook, libdrm, wayland, libxcb, libX11, ... }: stdenv.mkDerivation rec {
+    pname = "libmali-g610";
+    version = "g13p0";
     dontBuild = true;
     dontConfigure = true;
 
@@ -11,7 +11,7 @@
         hash = "sha256-VBk1D41we3re9qcjDurtnFZIduARNdwd6RnDir7Xr3o=";
     };
     nativeBuildInputs = [ autoPatchelfHook ];
-    buildInputs = [ stdenv.cc.cc.lib libdrm ];
+    buildInputs = [ stdenv.cc.cc.lib libdrm wayland libxcb libX11 ];
 
     installPhase = ''
         runHook preInstall
@@ -19,13 +19,13 @@
         mkdir -p $out/lib
         mkdir -p $out/etc/OpenCL/vendors
         mkdir -p $out/share/glvnd/egl_vendor.d
-        install --mode=555 lib/aarch64-linux-gnu/libmali-valhall-g610-g6p0-gbm.so $out/lib
-        echo $out/lib/libmali-valhall-g610-g6p0-gbm.so > $out/etc/OpenCL/vendors/mali.icd
+        install --mode=555 lib/aarch64-linux-gnu/libmali-valhall-g610-${version}-x11-wayland-gbm.so $out/lib
+        echo $out/lib/libmali-valhall-g610-${version}-x11-wayland-gbm.so > $out/etc/OpenCL/vendors/mali.icd
         cat > $out/share/glvnd/egl_vendor.d/40_mali.json << EOF
         {
           "file_format_version" : "1.0.0",
           "ICD" : {
-            "library_path" : "$out/lib/libmali-valhall-g610-g6p0-gbm.so"
+            "library_path" : "$out/lib/libmali-valhall-g610-${version}-x11-wayland-gbm.so"
           }
         }
         EOF
