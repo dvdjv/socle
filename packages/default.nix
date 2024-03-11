feb0308c9097e28267981c8db6ede76f576f3520
David Javakhishvili <david@underscore.dev>
2024-03-11 12:09:57 +0000

    Merge libmali and mali-firmware

--- packages/default.nix
+++ packages/default.nix
@@ -17,8 +17,11 @@
   orangepi-firmware = orangepi-firmware-2024_01_24;
   orangepi-firmware-2024_01_24 = (pkgs.callPackage ./orangepi-firmware.nix {});
 
-  mali-firmware = mali-firmware-g21p0_01eac0;
-  mali-firmware-g21p0_01eac0 = (pkgs.callPackage ./mali-firmware.nix {});
+  mali-firmware-g610 = mali-firmware-g610-g21p0-01eac0;
+  libmali-valhall-g610 = libmali-valhall-g610-g13p0;
+  libmali-valhall-g610-g13p0 = libmali-valhall-g610-g13p0-x11-wayland-gbm;
 
-  libmali = (pkgs.callPackage ./libmali.nix {});
+  inherit (pkgs.callPackage ./libmali.nix {})
+    mali-firmware-g610-g21p0-01eac0
+    libmali-valhall-g610-g13p0-x11-wayland-gbm;
 }
--- packages/libmali.nix
+++ packages/libmali.nix
@@ -1,35 +1,63 @@
-{ fetchFromGitHub, stdenv, autoPatchelfHook, libdrm, wayland, libxcb, libX11, ... }: stdenv.mkDerivation rec {
-    pname = "libmali-g610";
+{ stdenv, stdenvNoCC, fetchFromGitHub, autoPatchelfHook, libdrm, wayland, libxcb, libX11, ... }: let
+  src = fetchFromGitHub {
+    owner = "JeffyCN";
+    repo = "mirrors";
+    rev = "ab3d91e3df2ef1c487c2d8f69daea1729668e428";
+    hash = "sha256-VBk1D41we3re9qcjDurtnFZIduARNdwd6RnDir7Xr3o=";
+  };
+in {
+  mali-firmware-g610-g21p0-01eac0 = stdenvNoCC.mkDerivation {
+    pname = "mali-firmware";
+    version = "g21p0-01eac0";
+    dontBuild = true;
+    dontFixup = true;
+    compressFirmware = false;
+
+    inherit src;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/lib/firmware
+      install --mode=755 firmware/g610/mali_csffw.bin $out/lib/firmware/
+
+      runHook postInstall
+    '';
+  };
+
+  libmali-valhall-g610-g13p0-x11-wayland-gbm = stdenv.mkDerivation rec {
+    pname = "libmali-valhall-g610";
     version = "g13p0";
+    variant = "x11-wayland-gbm";
     dontBuild = true;
     dontConfigure = true;
 
-    src = fetchFromGitHub {
-        owner = "JeffyCN";
-        repo = "mirrors";
-        rev = "ab3d91e3df2ef1c487c2d8f69daea1729668e428";
-        hash = "sha256-VBk1D41we3re9qcjDurtnFZIduARNdwd6RnDir7Xr3o=";
-    };
+    inherit src;
+
     nativeBuildInputs = [ autoPatchelfHook ];
     buildInputs = [ stdenv.cc.cc.lib libdrm wayland libxcb libX11 ];
 
-    installPhase = ''
-        runHook preInstall
+    installPhase = let
+      libmaliFileName = "${pname}-${version}-${variant}.so";
+    in ''
+      runHook preInstall
 
-        mkdir -p $out/lib
-        mkdir -p $out/etc/OpenCL/vendors
-        mkdir -p $out/share/glvnd/egl_vendor.d
-        install --mode=555 lib/aarch64-linux-gnu/libmali-valhall-g610-${version}-x11-wayland-gbm.so $out/lib
-        echo $out/lib/libmali-valhall-g610-${version}-x11-wayland-gbm.so > $out/etc/OpenCL/vendors/mali.icd
-        cat > $out/share/glvnd/egl_vendor.d/40_mali.json << EOF
-        {
-          "file_format_version" : "1.0.0",
-          "ICD" : {
-            "library_path" : "$out/lib/libmali-valhall-g610-${version}-x11-wayland-gbm.so"
-          }
+      mkdir -p $out/lib
+      mkdir -p $out/etc/OpenCL/vendors
+      mkdir -p $out/share/glvnd/egl_vendor.d
+
+      install --mode=755 lib/aarch64-linux-gnu/${libmaliFileName} $out/lib
+      echo $out/lib/${libmaliFileName} > $out/etc/OpenCL/vendors/mali.icd
+      cat > $out/share/glvnd/egl_vendor.d/60_mali.json << EOF
+      {
+        "file_format_version" : "1.0.0",
+        "ICD" : {
+          "library_path" : "$out/lib/${libmaliFileName}"
         }
-        EOF
+      }
+      EOF
 
-        runHook postInstall
+      runHook postInstall
     '';
+  };
 }
