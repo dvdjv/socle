9f59084560b489383d80ca5461826d8e5759c2eb
David Javakhishvili <david@underscore.dev>
2025-12-19 10:32:36 +0000

    ffs

--- templates/orangepi-5x/flake.nix
+++ templates/orangepi-5x/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     socle = {
-      url = "github:dvdjv/socle";
+      url = "github:dvdjv/socle/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
