d7eb80a4e184324473aaa7d6503a467c055a0169
David Javakhishvili <david@underscore.dev>
2025-12-19 11:44:54 +0000

    copy from nixpkgs

--- modules/util/generic-extlinux-compatible/extlinux-conf-builder.nix
+++ modules/util/generic-extlinux-compatible/extlinux-conf-builder.nix
@@ -1,8 +1,14 @@
-{ pkgs }:
+{ lib, pkgs }:
 
-pkgs.substitute {
+pkgs.replaceVarsWith {
   src = ./extlinux-conf-builder.sh;
   isExecutable = true;
-  path = [pkgs.coreutils pkgs.gnused pkgs.gnugrep];
-  inherit (pkgs) bash;
+  replacements = {
+    path = lib.makeBinPath [
+      pkgs.coreutils
+      pkgs.gnused
+      pkgs.gnugrep
+    ];
+    inherit (pkgs) bash;
+  };
 }
